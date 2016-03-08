197ddeb19090cd8f7a72e4b195c6f5de2862e925
ScortiHuang <item0932@gmail.com>
2016-03-09 03:32:09 +0800

    試著修復work新增問題

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308172528) do
+ActiveRecord::Schema.define(version: 20160308193053) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "thing"
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 20160308172528) do
 
   create_table "works", force: :cascade do |t|
     t.string   "title"
-    t.boolean  "is_complete", default: true
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.boolean  "is_complete", default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.text     "description"
     t.integer  "user_id"
   end
