55f03fcd78025006844bcc5c309c76315d10c90f
ScortiHuang <item0932@gmail.com>
2016-03-09 04:29:06 +0800

    新增collapse 可以rwd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308193053) do
+ActiveRecord::Schema.define(version: 20160308200136) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "thing"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160308193053) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
